0e3904b982ce737d15f1d6190cc85a3ea0b0c879
ansonliam <ansonliam@users.noreply.github.com>
2026-08-24 18:55:02 +1000

    Group tray widget actions

--- src/AIUsageMonitor/Services/TrayIconService.cs
+++ src/AIUsageMonitor/Services/TrayIconService.cs
@@ -24,6 +24,7 @@ public sealed class TrayIconService : IDisposable
         var menu = new Forms.ContextMenuStrip();
         menu.Items.Add("Open Window Widget", null, (_, _) => _applicationController.ShowMainWindow());
         menu.Items.Add("Hide Window Widget", null, (_, _) => _applicationController.HideMainWindow());
+        menu.Items.Add(new Forms.ToolStripSeparator());
         menu.Items.Add("Open Taskbar Widget", null, (_, _) => _applicationController.SetTaskbarWidgetVisibility(true));
         menu.Items.Add("Hide Taskbar Widget", null, (_, _) => _applicationController.SetTaskbarWidgetVisibility(false));
         menu.Items.Add(new Forms.ToolStripSeparator());
